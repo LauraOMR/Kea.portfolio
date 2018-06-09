db5af54fb4f33a27e25adc02348041870cecbad4
LauraOMR <38040937+LauraOMR@users.noreply.github.com>
2018-06-09 16:30:05 +0200

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,20 @@
+const h2left = document.querySelector(".h2left");
+h2left.addEventListener("click", function () {
+    h2left.style.color = "salmon";
+});
+
+const h2right = document.querySelector(".h2right");
+h2right.addEventListener("click", function () {
+    h2right.style.color = "salmon";
+});
+
+const h2left3 = document.querySelector(".h2left3");
+h2left3.addEventListener("click", function () {
+    h2left3.style.color = "salmon";
+});
+
+const h2right3 = document.querySelector(".h2right3");
+h2right3.addEventListener("click", function () {
+    h2right3.style.color = "salmon";
+});
+
